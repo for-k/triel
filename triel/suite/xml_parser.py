08a0030361afd8dc5916fd5f4420cbea5bce29e1
ismael_PR <ismael@ismael>
2019-09-21 11:04:45 +0200

    waveform added

--- triel/suite/xml_parser.py
+++ triel/suite/xml_parser.py
@@ -3,19 +3,17 @@ import xml.etree.ElementTree as et
 import json
 
 class xml_parser:
-    def __init__ (self,xml_file,simulator):
-        self.xml_file = xml_file
+    def __init__ (self):
         self.json_out = ""
         self.data = {}
-        self.simulator = simulator
 
-    def parse(self):
-        tree = et.parse(self.xml_file)
+    def parse(self,xml_file):
+        tree = et.parse(xml_file)
         base = tree.getroot()
         return base
 
-    def coco_xml(self):
-        base = self.parse()
+    def coco_xml(self,xml_file,wave_file):
+        base = self.parse(xml_file)
         self.data["summary"]=[]
         self.data["test"]  =[]
         num_test=0
@@ -24,14 +22,14 @@ class xml_parser:
             num_test=num_test+1
             for std in cases:
                 num_fail=num_fail+1
-            self.data["test"].append({"classname":cases.attrib["classname"],"name":cases.attrib["name"],"time":cases.attrib["time"][0:6],"test":cases.attrib["classname"],"stdout":std.attrib["stdout"] if cases.findall('failure') else " Test passed","waveform":cases.attrib["classname"]})
+            self.data["test"].append({"classname":cases.attrib["classname"],"name":cases.attrib["name"],"time":cases.attrib["time"][0:6],"test":"Failed" if cases.findall('failure') else " OK","stdout":std.attrib["stdout"] if cases.findall('failure') else " Test passed","waveform":wave_file})
         self.data["summary"] = {"test":num_test,"failures":num_fail}
 
         self.json_out = json.dumps(self.data)
         return self.json_out
 
-    def vunit_xml(self):
-        base = self.parse()
+    def vunit_xml(self,xml_file,simulator):
+        base = self.parse(xml_file)
         self.data["summary"]=[]
         self.data["test"]  =[]
         self.data["summary"] = {"test":base.attrib["tests"],"failures":base.attrib["failures"]}
--- triel_test/test_xml_parser.py
+++ triel_test/test_xml_parser.py
@@ -3,7 +3,6 @@ from os.path import dirname
 sys.path.append('../triel/suite')
 import xml_parser
 
-vunitParser= xml_parser.xml_parser("./xml/out.xml","ghdl")
-cocoParser= xml_parser.xml_parser("./xml/results_fail.xml","ghdl")
-print(vunitParser.vunit_xml())
-print(cocoParser.coco_xml())
+xmlParser= xml_parser.xml_parser()
+print(xmlParser.vunit_xml("./xml/out.xml","ghdl"))
+print(xmlParser.coco_xml("./xml/results_fail.xml","./func.vcd"))
