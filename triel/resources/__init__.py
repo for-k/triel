af5a9afc4c48b535cd3dc8413a9cbd222d29475c
asaezper <alfredosaezperez@gmail.com>
2021-01-04 20:44:54 +0100

    Delete old triel

--- triel/resources/__init__.py
+++
@@ -1,6 +0,0 @@
-import os
-
-
-def resource_path(relative_path):
-    base_path = os.path.dirname(__file__)
-    return os.path.join(base_path, relative_path)
--- triel/server/manager/migrations/utils/db.py
+++
@@ -1,9 +0,0 @@
-def create(apps, model_str, **kwargs):
-    AbsModel = apps.get_model('manager', model_str)
-    result_list = AbsModel.objects.filter(**kwargs)
-    if not result_list:
-        result = AbsModel(**kwargs)
-        result.save()
-    else:
-        result = result_list[0]
-    return result
